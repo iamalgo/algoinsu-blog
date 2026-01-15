1dd947cd60d6011d5dc68ce1161ca9ae9962db83
Intelligent Algorithm Management <qruffin3428@gmail.com>
2026-01-15 17:29:28 -0600

    refactor: switch to official ghost npm package for stable deployment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-require('./ghost');
+const ghost = require('ghost');
+const path = require('path');
+
+ghost().then((ghostServer) => {
+    ghostServer.start();
+}).catch((err) => {
+    console.error(`Ghost failed to start: ${err.message}`);
+    process.exit(1);
+});
